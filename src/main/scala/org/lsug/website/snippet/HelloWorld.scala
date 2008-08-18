a04cb45e5de091de67478bd5e3333e44eaf7e7d0
Aaron K Roth <aaron.k.roth@gmail.com>
2008-08-18 21:12:38 +0000

    Initial modification to generic lift archetype-generated files.

--- src/main/scala/org/lsug/website/snippet/HelloWorld.scala
+++
@@ -1,6 +0,0 @@
-package org.lsug.website.snippet
-
-class HelloWorld {
-  def howdy = <span>Welcome to lsug-website at {new java.util.Date}</span>
-}
-
---
+++ src/main/scala/org/lsug/website/snippet/LSUGFrontPagePlaceholder.scala
@@ -0,0 +1,6 @@
+package org.lsug.website.snippet
+
+class LSUGFrontPagePlaceholder {
+def howdy = <span>Welcome to the London Scala Users Group website on {new java.util.Date}</span>
+}
+
