9728cf606143974e5686ead85e5a77a156a370ad
unknown <kevin@.(none)>
2009-07-06 23:32:55 +0100

    removed user model and database references

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -7,7 +7,6 @@ import net.liftweb.sitemap.Loc._
 import Helpers._
 import net.liftweb.mapper.{DB, ConnectionManager, Schemifier, DefaultConnectionIdentifier, ConnectionIdentifier}
 import java.sql.{Connection, DriverManager}
-import org.lsug.website.model._
  
 /**
   * A class that's instantiated early and run.  It allows the application
@@ -15,12 +14,9 @@ import org.lsug.website.model._
   */
 class Boot {
   def boot {
-    DefaultConnectionIdentifier.jndiName = "jdbc/scalausergroup"
-    
-    if (!DB.jndiJdbcConnAvailable_?) DB.defineConnectionManager(DefaultConnectionIdentifier, DBVendor)
     // where to search snippet
     LiftRules.addToPackages("org.lsug.website")
-    Schemifier.schemify(true, Log.infoF _, User)
+    //Schemifier.schemify(true, Log.infoF _, User)
 
     // Build SiteMap
     val entries =
@@ -30,19 +26,5 @@ class Boot {
       // :: User.sitemap
     LiftRules.setSiteMap(SiteMap(entries:_*))
   }
-}
-
-
-object DBVendor extends ConnectionManager {
-  def newConnection(name: ConnectionIdentifier): Box[Connection] = {
-    try {
-      Class.forName("org.apache.derby.jdbc.EmbeddedDriver")
-      val dm = DriverManager.getConnection("jdbc:derby:lift_example;create=true")
-      Full(dm)
-    } catch {
-      case e : Exception => e.printStackTrace; Empty
-    }
-  }
-  def releaseConnection(conn: Connection) {conn.close}
 }
 
